09a9ab118f3d3c7dbaef5a4fb9be85afbfefffca
Erica Mason <em006@uark.edu>
2019-04-03 18:42:46 -0500

    replace SettingScreen w/ ProfileScreen

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -5,7 +5,7 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
-import LinksScreen from '../screens/ProfileScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 //import SettingsScreen from '../screens/SettingsScreen';
 
 const HomeStack = createStackNavigator({
