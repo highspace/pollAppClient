e90f4bc37fb7156925773ffc7b850d3d292eede3
Erica Mason <em006@uark.edu>
2019-04-03 20:20:56 -0500

    profile header

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -14,10 +14,10 @@ import { WebBrowser } from 'expo';
 import { MonoText } from '../components/StyledText';
 
 export default class ProfileScreen extends React.Component {
-//    static navigationOptions = {
-//        //header: null,
-//        title: 'User Profile',
-//    };
+    static navigationOptions = {
+        //header: null,
+        title: 'User Profile',
+    };
     render() {
         return (
           //<ScrollView style={styles.container}>
@@ -32,7 +32,7 @@ export default class ProfileScreen extends React.Component {
 
                     </View>
 
-                    <Text style = {styles.username}>Username Here</Text>
+                    //<Text style = {styles.username}>Username Here</Text>
 
                 </View>
             </Image>
