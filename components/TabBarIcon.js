60fe925f0108898fc484fed505e146ac63829ab5
Erica Mason <em006@uark.edu>
2019-04-03 19:32:33 -0500

    profilescreen icon

--- components/TabBarIcon.js
+++ components/TabBarIcon.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Icon } from 'expo';
 
 import Colors from '../constants/Colors';
+//import Image from '../assets/images';
 
 export default class TabBarIcon extends React.Component {
   render() {
@@ -11,6 +12,7 @@ export default class TabBarIcon extends React.Component {
         size={26}
         style={{ marginBottom: -3 }}
         color={this.props.focused ? Colors.tabIconSelected : Colors.tabIconDefault}
+        icon={require('./assets/images/profileicon.png')}
       />
     );
   }
